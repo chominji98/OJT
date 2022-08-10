2ca39a68f0df9ca7e9c21c2b0a162f1f39f128da
chominji98 <chominji981114@gmail.com>
2022-08-10 18:59:37 +0900

    oAuthSchema 수정

--- oAuthSchema.js
+++ oAuthSchema.js
@@ -3,22 +3,36 @@ import mongoose from "mongoose";
 const { Schema, } = mongoose;
 
 const oAuthSchema = new Schema({
+  responseType: {
+    type: String,
+    required: true,
+  },
   clientId: {
     type: String,
     required: true,
   },
-  clientPw: {
+  clientSecret: {
     type: String,
     required: true,
   },
   redirectUrl: {
     type: String,
   },
+  grantType: {
+    type: String,
+    required: true,
+  },
   scope: {// 요청 접근 범위
     type: String,
   },
+  state: {
+    //클라이언트가 요청과 콜백 사이의 상태를 유지하기 위해 사용하는 값.
+    //인증 서버는 사용자 에이전트를 클라이언트로 다시 리다이렉션할 때 이 값을 포함함.
+    type: String,
+  },
   authorizationCode: {
-    type: Object,
+    type: String,
+    required: true,
   },
   accessToken: {
     type: String,
@@ -30,8 +44,8 @@ const oAuthSchema = new Schema({
   expires_in: {//만료 기간
     type: Date,
     required: true,
-  }
-  //만료 기간, 사용 기간
+  },
+
 
 });
 
